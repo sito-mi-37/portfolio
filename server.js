e372e57f5996ac4bf349189864b908de3bbc0788
sito <sito.mi.37@gmail.com>
2022-10-28 15:34:56 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+    let filepath = path.join(__dirname,'public', request.url === '/home' ? 'home.html' : request.url)
+    let contentType = getContentType(filepath) || 'text/html'
+    fs.readFile(filepath, 'utf8', (err, data)=>{
+        if(err) {
+            // if(err.code === 'ENOENT'){
+            //     response.writeHead(200, {'contentType': contentType})
+
+            // }
+            console.log(err)
+        }
+        response.writeHead(200, {'Content-Type': contentType})
+        response.end(data)
+    })
+     
+})
+
+const getContentType = (filepath) =>{
+    let extname = path.extname(filepath)
+    if(extname === '.js'){
+        return 'text/javascript'
+    }
+    if(extname === '.css'){
+        return 'text/css'
+    }
+}
+const port = 5000
+server.listen(port, ()=>{
+    console.log('server is listning ')
+})
